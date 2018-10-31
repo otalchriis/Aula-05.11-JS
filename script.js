138f028f70958ac91678786b678b005664a6614a
otalchriis <christian_patua@yahoo.com.br>
2018-10-30 22:00:32 -0300

    Primeiros passos com JS

---
+++ script.js
@@ -0,0 +1,27 @@
+// let nomeDoUsuario = "Christian Oliveira Santos";
+// console.log("Hello World!")
+// // alert("Hello World!")
+
+// let soma = 1+1;
+// let nome = "Christian O. Santos";
+// let vivo = false;
+// let idade = 25;
+
+// // let saida1 = "Olá " + nome + "! Tudo bem com voce?";
+// let saida1 = `Olá ${nome}! você tem ${idade} anos`;
+
+// console.log(saida1);
+
+// console.log(prompt("qual seu nome?"));
+// console.log(idade)
+// vivo = confirm("Voce está Vivo")
+// console.log(`Você está vivo? ${vivo}`)
+
+// RECEBA 2 VALORES DO USUÁRIO E RETORNE UM ALERT COM O RESULTADO DA SOMA
+
+// console.log(`Você está vivo? ${vivo}`)
+
+let idade = Number(prompt("Digite sua idade"));
+let idade2 = Number(prompt("Digite sua idade2"));
+
+alert(idade + idade2);
